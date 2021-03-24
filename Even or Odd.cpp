e17eb8151c4e78d403edaab830ea14cc37fcb8b6
UpamanyuGhosh <76256514+UpamanyuGhosh@users.noreply.github.com>
2021-03-24 19:51:35 +0530

    Add files via upload

---
+++ Even or Odd.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main(){int a1,a2,a3;
+printf("Enter 1st Gaurdians Age");
+scanf("\n %d",&a1);
+printf("\n Enter 2nd Gaurdians Age");
+scanf("\n %d",&a2);
+printf("\n Enter Your Age");
+scanf("\n %d",&a3);
+	if(a1>a2 && a3){
+		printf("\n %d is older than %d",a1,a2,a3);
+		}else if(a2>a1 && a3){
+		printf("\n %d is older than %d", a2,a1,a3);}
+		if(a3>a2 && a1){
+		printf("\n %d is older than %d");
+		}
+		if(a1==a2==a3){
+		printf("\n %d is equal to %d",a1,a2,a3);
+		}}
