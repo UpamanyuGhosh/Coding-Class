8d6b20377a84e329930bec95909d3588af40ab8b
UpamanyuGhosh <76256514+UpamanyuGhosh@users.noreply.github.com>
2021-10-10 11:59:34 +0530

    Add files via upload

---
+++ javaHW.java
@@ -0,0 +1,7 @@
+ class javaHW {
+    public static void main(String args[]) {
+        System.out.println("Father's Name Is Saurav Kr. Ghosh");
+        System.out.println("Mother's Name Is Suparna Ghosh");
+        System.out.println("My Name Is Upamanyu Ghosh");
+    }
+}
