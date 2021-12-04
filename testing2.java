2db2e3b9c766685808216a2669a3d06078aeee43
UpamanyuGhosh <76256514+UpamanyuGhosh@users.noreply.github.com>
2021-12-04 21:27:27 +0530

    Update testing2.java

--- testing2.java
+++ testing2.java
@@ -3,9 +3,9 @@ public static void main(int a) {
 System.out.println("Please Enter A Number"+a);
 int copy = a;
 
-    double i , precision = 0.1;
+    double i , x = 0.1;
       for(i = 1; i * i  <= a ; i++);
-      for(--i ; i * i < a ; i += precision);
+      for(--i ; i * i < a ; i += x);
       System.out.println("Square root of given number  "+copy+ " is "+ i);
 }
 }
